35bf6f50ba6400849949e156d5fa833420b7e3b9
Karan Vadhan <karanvadhan789@gmail.com>
2023-09-03 15:08:35 -0500

    quick api changes

--- app/src/main/java/com/example/placesnearme/api/PlacesApi.kt
+++ app/src/main/java/com/example/placesnearme/api/PlacesApi.kt
@@ -1,5 +1,6 @@
 package com.example.placesnearme.api
 
+import com.example.placesnearme.model.Images
 import com.example.placesnearme.model.Places
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -7,6 +8,8 @@ import retrofit2.http.Path
 interface PlacesApi {
 
     @GET("data/places")
-    suspend fun getPlaces(): List<Places>
+    suspend fun getPlaces(): Places
 
+    @GET("data/img/{id}")
+    suspend fun getImageById(@Path("id") id: String): Images
 }
---
+++ app/src/main/java/com/example/placesnearme/model/Images.kt
@@ -0,0 +1,7 @@
+package com.example.placesnearme.model
+
+data class Images(
+    val done: Boolean,
+    val img: String,
+    val placeId: String
+)
