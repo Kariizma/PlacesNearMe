44323b400170ee733e8d89ee5c52c4a59418334b
Karan Vadhan <karanvadhan789@gmail.com>
2023-09-03 15:55:09 -0500

    i think part 1 is done

--- app/src/main/java/com/example/placesnearme/view/MainActivity.kt
+++ app/src/main/java/com/example/placesnearme/view/MainActivity.kt
@@ -1,8 +1,11 @@
+@file:OptIn(ExperimentalCoilApi::class)
+
 package com.example.placesnearme.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -10,17 +13,22 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.placesnearme.R
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.rememberImagePainter
 import com.example.placesnearme.model.Place
 import com.example.placesnearme.ui.theme.PlacesNearMeTheme
+import com.example.placesnearme.viewmodel.PlacesViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: PlacesViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -29,24 +37,16 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val mockPlaces = listOf(
-                        Place(
-                            name = "Beautiful Place",
-                            id = "aaa",
-                            address = "123 Compose Street",
-                            price = "$$",
-                            reviews = 20,
-                            stars = 3
-                        )
-                    )
-                    PlacesList(places = mockPlaces)
+                    val places = viewModel.places.collectAsState().value
+                    val imagesMap = viewModel.imagesMap.collectAsState().value
+                    PlacesList(places = places, imagesMap = imagesMap)
                 }
             }
         }
     }
 
     @Composable
-    fun PlacesList(places: List<Place>) {
+    fun PlacesList(places: List<Place>, imagesMap: Map<String, String>) {
         LazyColumn {
             items(places) { place ->
                 PlaceListItem(
@@ -55,14 +55,14 @@ class MainActivity : ComponentActivity() {
                     price = place.price,
                     reviews = place.reviews,
                     stars = place.stars,
-                    imageResId = R.drawable.ic_launcher_foreground,
+                    imageUrl = imagesMap[place.id]
                 )
             }
         }
     }
 
     @Composable
-    fun PlaceListItem(name: String, address: String, price: String, reviews: Int, stars: Int, imageResId: Int) {
+    fun PlaceListItem(name: String, address: String, price: String, reviews: Int, stars: Int, imageUrl: String?) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
@@ -70,13 +70,15 @@ class MainActivity : ComponentActivity() {
                 .background(MaterialTheme.colorScheme.primary)
                 .padding(16.dp)
         ) {
-            Image(
-                painter = painterResource(id = imageResId),
-                contentDescription = "Place Image",
-                modifier = Modifier
-                    .size(60.dp, 60.dp)
-                    .background(MaterialTheme.colorScheme.onPrimary)
-            )
+            imageUrl?.let {
+                Image(
+                    painter = rememberImagePainter(data = it),
+                    contentDescription = "Place Image",
+                    modifier = Modifier
+                        .size(60.dp, 60.dp)
+                        .background(MaterialTheme.colorScheme.onPrimary)
+                )
+            }
             Spacer(modifier = Modifier.width(16.dp))
             Column {
                 Text(text = name, style = MaterialTheme.typography.headlineSmall)
@@ -99,6 +101,7 @@ class MainActivity : ComponentActivity() {
                 stars = 3,
             )
         )
-        PlacesList(places = mockPlaces)
+        val mockImagesMap = mapOf("aaa" to "https://example.com/image.jpg")
+        PlacesList(places = mockPlaces, imagesMap = mockImagesMap)
     }
 }
