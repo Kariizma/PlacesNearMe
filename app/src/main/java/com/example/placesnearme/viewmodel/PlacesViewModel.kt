ebce6da96d07ac211c9da2ac43bd7814883df4ad
Karan Vadhan <karanvadhan789@gmail.com>
2023-09-03 15:18:44 -0500

    trying to map the images to its ids

--- app/src/main/java/com/example/placesnearme/viewmodel/PlacesViewModel.kt
+++ app/src/main/java/com/example/placesnearme/viewmodel/PlacesViewModel.kt
@@ -5,7 +5,7 @@ import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.placesnearme.api.PlacesApi
-import com.example.placesnearme.model.Places
+import com.example.placesnearme.model.Place
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -17,7 +17,11 @@ class PlacesViewModel
 @Inject constructor(private val placesApi: PlacesApi, application: Application):
     AndroidViewModel(application) {
 
-    private val _places = MutableStateFlow<List<Places>>(listOf())
+    private var _places = MutableStateFlow<List<Place>>(listOf())
+    val places: StateFlow<List<Place>> = _places
+
+    private val _imagesMap = MutableStateFlow<Map<String, String>>(emptyMap())
+    val imagesMap: StateFlow<Map<String, String>> = _imagesMap
 
     init {
         getPlaces()
@@ -28,10 +32,18 @@ class PlacesViewModel
         viewModelScope.launch {
             Log.v("API Data:", "Loading...")
             try {
-                placesApi.getPlaces()
+                val placesList = placesApi.getPlaces().places
+                val imagesMapTemp = mutableMapOf<String, String>()
+
+                placesList.forEach { place ->
+                    val imageResponse = placesApi.getImageById(place.id)
+                    imagesMapTemp[place.id] = imageResponse.img
+                }
+
+                _places.value = placesList
+                _imagesMap.value = imagesMapTemp
                 Log.v("API Data:", "Success")
-            } catch (e: Exception)
-            {
+            } catch (e: Exception) {
                 Log.e("API Data:", "Error: $e")
             }
         }
